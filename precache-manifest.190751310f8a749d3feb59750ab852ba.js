b814784f944405f8816156d3a0e53ecc9ce588e4
YoduYodu <wang7ong@gmail.com>
2019-09-05 18:00:49 -0700

    Updates

---
+++ precache-manifest.190751310f8a749d3feb59750ab852ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccddf34a70e639f0f305a16e1b1b4fec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e9b694f973365ed9a26",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "02051a41d448c3da6c9c",
+    "url": "/static/js/2.060c3e4c.chunk.js"
+  },
+  {
+    "revision": "0e9b694f973365ed9a26",
+    "url": "/static/js/main.f40b8194.chunk.js"
+  },
+  {
+    "revision": "c3bc15d8467a25474934",
+    "url": "/static/js/runtime~main.8ef4ebb3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
