2cd7b75590d5fcbbfaea00c31fdad16defe9dfd9
yoduyodu <wang7ong@gmail.com>
2020-02-26 10:29:58 -0800

    Change text

--- src/views/ProfilePage/ProfilePage.js
+++ src/views/ProfilePage/ProfilePage.js
@@ -78,10 +78,10 @@ export default function ProfilePage(props) {
             </GridContainer>
             <div className={classes.description}>
               <p>
-              Graduating in May 2021, with development experience in big/medium/startup tech industry. <br/> A developer with proficiency in Java, C/C++, Python, GO.
+              Graduating in May 2021, with development experience in big/medium/startup tech companies.(See my Résumé) <br/> A developer with proficiency in Java, C/C++, Python, GO.
               </p>
               <p>
-                Visual Thinker, Adventure Lover, Lazy Hustler{" "}
+                Visual Thinker, Adventure Lover, Idly Diligent{" "}
               </p>
             </div>
             <GridContainer justify="center">
